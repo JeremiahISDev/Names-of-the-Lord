06ed30b4a20b434dc0ed812e1f9a68d9320f1b19
JeremiahISamuelMMS <jr.isamuel08@gmail.com>
2021-12-19 19:26:58 +0000

    tailcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <h1 class="font-serif text-3xl pt-6 text-center text-royalb font-bold underline decoration-sky-500/30 hover:decoration-sky-500 hover:decoration-dashed">
+		<a href="/"> Names Of Our Lord</a>
+	</h1>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
