078c613ca72099a25fb6038513df164e72c19b4c
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2021-12-19 19:57:18 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
   <>
+	  <Head>
+	  <title>The Names Of Our Lord</title>
+	  </Head>
     <h1 class="font-serif text-3xl pt-6 text-center text-royalb font-bold underline decoration-sky-500/30 hover:decoration-sky-500 hover:decoration-dashed">
 		<a href="/"> Names Of Our Lord</a>
 	</h1>
